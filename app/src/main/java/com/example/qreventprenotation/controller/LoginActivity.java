b87f16359c5b10e34f96638f29c30e5d31522e20
giorgio.dellemonache@studenti.unipg.it <57790299+GiorgioDm@users.noreply.github.com>
2021-07-01 17:39:04 +0200

    I° esecuzione avvenuta con successo

--- app/src/main/java/com/example/qreventprenotation/controller/LoginActivity.java
+++ app/src/main/java/com/example/qreventprenotation/controller/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.qreventprenotation.R;
 
@@ -20,14 +21,23 @@ public class LoginActivity extends AppCompatActivity {
 
         usernameLogin = findViewById(R.id.lgn_username);
         passwordLogin = findViewById(R.id.lgn_password);
+
+        TextView txtvw = (TextView) findViewById(R.id.textView4);
+        txtvw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openRegister = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(openRegister);
+            }
+        });
     }
 
     public void login(View view) {
         //query database
     }
 
-    public void openRegister(View view) {
+    /*public void openRegister(View view) {
         Intent openRegister = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(openRegister);
-    }
+    }*/
 }

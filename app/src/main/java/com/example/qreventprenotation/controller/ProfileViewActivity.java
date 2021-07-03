ab169d05269778965dac2b70304086936742cf04
giorgio.dellemonache@studenti.unipg.it <57790299+GiorgioDm@users.noreply.github.com>
2021-07-03 15:16:38 +0200

    Aggiunta schermata menu principale (completata) Aggiunta schermata gestione profilo (completata) Aggiunta schermata cambia password (completata)

---
+++ app/src/main/java/com/example/qreventprenotation/controller/ProfileViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.qreventprenotation.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.qreventprenotation.R;
+
+public class ProfileViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_view);
+    }
+}

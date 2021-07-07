86195c2bd982fb3453e3f6624570613a6d8ee15e
Cerrativan <ivan.disante@studenti.unipg.it>
2021-07-07 17:13:13 +0200

    Change password redirect added

--- app/src/main/java/com/example/qreventprenotation/controller/ProfileViewActivity.java
+++ app/src/main/java/com/example/qreventprenotation/controller/ProfileViewActivity.java
@@ -2,15 +2,30 @@ package com.example.qreventprenotation.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.qreventprenotation.R;
 
 public class ProfileViewActivity extends AppCompatActivity {
 
+    TextView pswChange;
+    Intent openPsWChange;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+
+        pswChange = findViewById(R.id.textViewChangePsw);
+        pswChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPsWChange = new Intent(ProfileViewActivity.this, ChangePasswordActivity.class);
+                startActivity(openPsWChange);
+            }
+        });
     }
 }
